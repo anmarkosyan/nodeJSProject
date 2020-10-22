6dd76932d4d9fd98d727e7ed04bfe414d04dcaf6
anmarkosyan <anush.markos@gmail.com>
2020-10-22 15:22:31 +0400

    how to create server and listen it

--- 1-node-farm/main.js
+++ 1-node-farm/main.js
@@ -1,6 +1,9 @@
 
 const fs = require('fs');
+const http = require('http');
 
+
+//============= FILE SYSTEM =============================
 // blocking, Synchronous way
 // const txtIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(txtIn);
@@ -9,21 +12,32 @@ const fs = require('fs');
 //
 // console.log('file written');
 
-
 //non-blocking, Asynchronous way
- fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-     if(err) return console.log('ERROR ❗️');
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3)
+//  fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//      if(err) return console.log('ERROR ❗️');
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3)
+//
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+//                 console.log('Your file is written 😀')
+//             });
+//         })
+//     });
+//  });
+// console.log('Will read file!');
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
-                console.log('Your file is written 😀')
-            });
-        })
-    });
- });
-console.log('Will read file!');
+
+//============== SERVER ==================
+//creating a server
+const server = http.createServer((req, res) => {
+    res.end('hello from the server!');
+});
+
+//listen to incoming requests from the client
+server.listen(8000,'127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
 
 

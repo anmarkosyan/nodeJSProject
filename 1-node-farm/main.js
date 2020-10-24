b642366f0aec305da6a0ae40e9890e3c54a78d6c
anmarkosyan <anush.markos@gmail.com>
2020-10-24 16:32:47 +0400

    building product page, and finish node product

--- 1-node-farm/main.js
+++ 1-node-farm/main.js
@@ -55,10 +55,13 @@ const dataObj = JSON.parse(data);
 
 //creating a server
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    // console.log(req.url);
+    // console.log(url.parse(req.url));
+
+    const {query, pathname}  = url.parse(req.url, true);
 
     //overview page
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {'Content-type': 'text/html'});
         //replace the placeholders in the template with the actual data from the current product
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -67,12 +70,14 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
      //product page
-    }else if(pathName === '/product'){
+    }else if(pathname === '/product'){
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(tempProduct);
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
     //API
-    }else if(pathName === '/api'){
+    }else if(pathname === '/api'){
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
 

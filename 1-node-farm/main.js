492048200bc9a5c2bec3386075ef7cd64d934ac5
anmarkosyan <anush.markos@gmail.com>
2020-10-24 16:49:36 +0400

    added our own module file within main file

--- 1-node-farm/main.js
+++ 1-node-farm/main.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //============= FILE SYSTEM =============================
 // blocking, Synchronous way
@@ -31,19 +31,7 @@ const url = require('url');
 
 
 //============== SERVER ==================
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRITPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
 //we put outside and put it in Sync method, this outside code called top-level code
 //is only ever executed once we start the program,
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');

645d7d0459a1533551b9de0842293d8e5928f628
anmarkosyan <anush.markos@gmail.com>
2020-10-27 19:20:09 +0400

    do some changes

--- 1-node-farm/modules/replaceTemplate.js
+++ 1-node-farm/modules/replaceTemplate.js
@@ -5,7 +5,7 @@ module.exports = (temp, product) => {
     output = output.replace(/{%FROM%}/g, product.from);
     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRITPTION%}/g, product.description);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
 
     if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');

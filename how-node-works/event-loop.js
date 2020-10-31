5711902a1c5bdd0b9bd5adffc712fd20509acb69
anmarkosyan <anush.markos@gmail.com>
2020-10-31 17:24:13 +0400

    how to create thread pool

--- how-node-works/event-loop.js
+++ how-node-works/event-loop.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
 
 setTimeout(() => console.log("Timer 1 finished!"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
@@ -12,6 +15,19 @@ fs.readFile("test-file.txt", () => {
   setImmediate(() => console.log("Immediate 2 finished"));
 
   process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
 });
 
 console.log("Hello from the top-level-code");

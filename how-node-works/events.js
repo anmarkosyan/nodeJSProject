061d449a8c439759aa7fc0ec14462b9a44d4a81c
anmarkosyan <anush.markos@gmail.com>
2020-11-02 18:30:00 +0400

    how to create small web server and listen to the events that it emits

--- how-node-works/events.js
+++ how-node-works/events.js
@@ -1,5 +1,7 @@
 //call built-in node module 'event', using EventEmitter standard name
 const EventEmitter = require("events");
+//for #2 example
+const http = require("http");
 
 //for best practise in real life,  we should create new class, that will actually inherit
 //from the node EventEmitter
@@ -28,3 +30,22 @@ myEmitter.on("newSale", (stock) => {
 
 //object that emits named events
 myEmitter.emit("newSale", 9);
+
+//=================== #2 example - create small web server and than listen to the event that it emits ==========
+
+const server = http.createServer();
+//listen events, that server will emit
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  res.end("Request received!");
+});
+server.on("request", (req, res) => {
+  console.log("Another request 😀");
+});
+server.on("close", () => {
+  console.log("Server closed!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("withing for request...");
+});

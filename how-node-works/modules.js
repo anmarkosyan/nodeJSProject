88a2d4c4df45d08de9349853f7d5e4eaba519dc1
anmarkosyan <anush.markos@gmail.com>
2020-11-03 16:20:21 +0400

    how to use exports shorthand

--- how-node-works/modules.js
+++ how-node-works/modules.js
@@ -5,3 +5,11 @@
 const Calc = require("./test-modul-1");
 const calc1 = new Calc();
 console.log(calc1.add(2, 5));
+
+//exports
+//#1
+// const calc2 = require("./test-module-2");
+// console.log(calc2.add(2, 5));
+//#2
+const {add, multiply, divide} = require('./test-module-2');
+console.log(multiply(2, 5));

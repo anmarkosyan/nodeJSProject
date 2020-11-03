9fae5c740d6081e50e38175e4d256b0bb7d7f569
anmarkosyan <anush.markos@gmail.com>
2020-11-03 15:32:57 +0400

    solution #2 using streams

--- how-node-works/streams.js
+++ how-node-works/streams.js
@@ -4,10 +4,24 @@ const server = require("http").createServer();
 
 server.on("request", (req, res) => {
   //solution #1, and not the best, because it take a long time until file totally load into the memory and than it can send the data.
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) throw err;
-    res.end(data);
+  // fs.readFile("test-file.txt", (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  //solution #2:using streams
+  const readable = fs.createReadStream("test-fileeee.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk);
   });
+  readable.on("end", () => {
+    res.end();
+  });
+  readable.on('error', err => {
+      console.log(err);
+      res.statusCode = 500;
+      res.end('File not found!');
+  })
 });
 
 server.listen(8000, "127.0.0.1", () => {

d8c8e2eb0b0ced67c789479760ad7b996f995dac
anmarkosyan <anush.markos@gmail.com>
2020-11-23 17:43:42 +0400

    how to create route handler with GET request

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,8 +1,10 @@
 //how to use express web framework
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 //define route, http method for the request
+/*
 app.get('/', (req, res) => {
   res
     .status(200)
@@ -11,6 +13,25 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send('You can post in this endpoint...');
+});
+
+ */
+
+//implement the tours route and handling  GET method, and create a route handler function
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//what do we want to do when someone hits this route?
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    //and formatted our response using JSend specification
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 //listen the server

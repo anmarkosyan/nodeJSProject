c4c13e04b6b6e049ad6c380f82a904e9a6064a78
anmarkosyan <anush.markos@gmail.com>
2020-11-27 19:33:23 +0400

    prettier changes

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController')
-
-
+const tourController = require('./../controllers/tourController');
 
 // ROUTES
 // MOUNTING the router
 const router = express.Router();
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const userController = require('./../controllers/userController')
+const userController = require('./../controllers/userController');
 
 // ROUTES
 // MOUNTING the router
 const router = express.Router();
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;

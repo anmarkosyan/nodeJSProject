ae5191f647d57456a778ad6a4248a64c0c3f70a7
anmarkosyan <anush.markos@gmail.com>
2020-11-27 19:09:40 +0400

    remove controllers in a different files

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,101 +1,13 @@
-const fs = require('fs');
 const express = require('express');
+const tourController = require('./../controllers/tourController')
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
 
-//2: ROUTE HANDLERS
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    //and formatted our response using JSend specification
-    status: 'success',
-    result: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours,
-    },
-  });
-};
 
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id === id);
-
-  //how to define an error
-  //if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      //status 201 means created and 200 stands for OK
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here ...>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  //204 status means: no content
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-//3: ROUTES
+// ROUTES
 // MOUNTING the router
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,47 +1,11 @@
 const express = require('express');
+const userController = require('./../controllers/userController')
 
-//👤 users route handler
-const getAllUsers = (req, res) => {
-  //500 status: internal server error
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not yet defined!',
-  });
-};
-const getUser = (req, res) => {
-  //500 status: internal server error
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not yet defined!',
-  });
-};
-const createUser = (req, res) => {
-  //500 status: internal server error
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not yet defined!',
-  });
-};
-const updateUser = (req, res) => {
-  //500 status: internal server error
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not yet defined!',
-  });
-};
-const deleteUser = (req, res) => {
-  //500 status: internal server error
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not yet defined!',
-  });
-};
-
-//3: ROUTES
+// ROUTES
 // MOUNTING the router
 const router = express.Router();
 
-router.route('/').get(getAllUsers).post(createUser);
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
 module.exports = router;

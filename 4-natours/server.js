9c0eca06b4824dd64de7c982fb15c995719f3834
anmarkosyan <anush.markos@gmail.com>
2020-11-27 19:30:07 +0400

    server file

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -4,10 +4,10 @@ and then everything that is related to the server in another main file.
 So starting now, server.js will actually be our starting file where everything starts,
 and it's there when we listen to our server.
  */
-const app = require('./app')
+const app = require('./app');
 
 //START SERVER
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });

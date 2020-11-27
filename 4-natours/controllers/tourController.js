77d6b669f3c6478590968d23faebc62ebdb04d2c
anmarkosyan <anush.markos@gmail.com>
2020-11-27 19:08:22 +0400

    how to create controllers and connect them with routes

---
+++ 4-natours/controllers/tourController.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+// ROUTE HANDLERS
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+        //and formatted our response using JSend specification
+        status: 'success',
+        result: tours.length,
+        requestedAt: req.requestTime,
+        data: {
+            tours,
+        },
+    });
+};
+
+exports.getTour = (req, res) => {
+    const id = req.params.id * 1;
+
+    const tour = tours.find((el) => el.id === id);
+
+    //how to define an error
+    //if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    });
+};
+
+exports.createTour = (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            //status 201 means created and 200 stands for OK
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
+};
+
+exports.updateTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here ...>',
+        },
+    });
+};
+
+exports.deleteTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+    //204 status means: no content
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+};

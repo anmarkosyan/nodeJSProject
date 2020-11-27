60604b452c2a89709e58a2daa1cae2937ae32538
anmarkosyan <anush.markos@gmail.com>
2020-11-27 19:22:28 +0400

    created server file for separate express files and global server files

---
+++ 4-natours/server.js
@@ -0,0 +1,13 @@
+/*
+it's a good practice to have everything that is related to express in one file,
+and then everything that is related to the server in another main file.
+So starting now, server.js will actually be our starting file where everything starts,
+and it's there when we listen to our server.
+ */
+const app = require('./app')
+
+//START SERVER
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});

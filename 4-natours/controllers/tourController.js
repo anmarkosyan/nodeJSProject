ede7845aac6db880294fbc1494d3692b4575eb36
anmarkosyan <anush.markos@gmail.com>
2020-11-29 20:31:51 +0400

    created checkBody() middleware

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -14,6 +14,16 @@ exports.checkID = (req, res, next, val) => {
   }
   next();
 };
+//created checkBody middleware for post request
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
 // ROUTE HANDLERS
 exports.getAllTours = (req, res) => {
   res.status(200).json({

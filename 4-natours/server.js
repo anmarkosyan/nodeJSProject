f9ec04e52c7aea7f604823e38dd5f7d8d7b5df7d
anmarkosyan <anush.markos@gmail.com>
2020-12-01 15:47:52 +0400

    created environment variable file config.env and intall dotenv npm package

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -4,7 +4,10 @@ and then everything that is related to the server in another main file.
 So starting now, server.js will actually be our starting file where everything starts,
 and it's there when we listen to our server.
  */
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 //console.log(app.get('env'));//to see express environment
 //to see all node.js environment

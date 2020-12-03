6b7b25a36a520a0ee93077da5a3a8046e5646d95
anmarkosyan <anush.markos@gmail.com>
2020-12-03 13:14:46 +0400

    #1: we create DATABASES for local(just for show how to do it, I  will delete it in the next) and hoste databases in the config environment,2:  intall Mangoose driver for interact node code with MongoDB database,3: using Mongoose package connected it

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -4,6 +4,7 @@ and then everything that is related to the server in another main file.
 So starting now, server.js will actually be our starting file where everything starts,
 and it's there when we listen to our server.
  */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -12,6 +13,28 @@ const app = require('./app');
 //console.log(app.get('env'));//to see express environment
 //to see all node.js environment
 //console.log(process.env);
+
+//how to connect app to the hosted database (to the cloud MongoDB Atlas)
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection'));
+
+//how to connect app to the local database
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log('DB local connection'));
 
 //START SERVER
 const port = process.env.PORT || 3000;

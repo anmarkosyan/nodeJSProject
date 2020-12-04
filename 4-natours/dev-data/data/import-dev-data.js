b15258e0433f90498ca8992f6983d0dcd210e6a2
anmarkosyan <anush.markos@gmail.com>
2020-12-04 19:45:42 +0400

    how to import all the data to the database, that we have in tours.simple.json file

---
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection'));
+
+//reade json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//import data to the database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded 😀');
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit();
+};
+
+//delete all data from the database
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted 😀');
+
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+//console.log(process.argv)

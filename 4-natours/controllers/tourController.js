66833f915276e45d664adbc6bb4434acd2d3679c
anmarkosyan <anush.markos@gmail.com>
2020-12-04 11:42:08 +0400

    here we required tourModel.js file, and no longer to need fs file, and tours var, so we will start implementing the correct functions in our API

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,19 +1,9 @@
-const fs = require('fs');
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tourModel');
 
-//created checkId middleware
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is : ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
+
 //created checkBody middleware for post request
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -30,47 +20,35 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     //and formatted our response using JSend specification
     status: 'success',
-    result: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours,
-    },
+    // result: tours.length,
+    // requestedAt: req.requestTime,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const tour = tours.find((el) => el.id === id);
+  //
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
   //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      //status 201 means created and 200 stands for OK
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {

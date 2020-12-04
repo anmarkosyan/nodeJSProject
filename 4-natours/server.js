41bb5dda1266e3a4c05e07e6a91af40a2a01b555
anmarkosyan <anush.markos@gmail.com>
2020-12-04 13:29:27 +0400

    change start:env pointer within package.json, change PORT number 3000 within config.env, and save new port within server.js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -28,7 +28,7 @@ mongoose
   .then(() => console.log('DB connection'));
 
 //START SERVER
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });

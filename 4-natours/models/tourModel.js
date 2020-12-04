bb7cb16de46570e690a20eaefd2e1da5120f63e6
anmarkosyan <anush.markos@gmail.com>
2020-12-04 18:59:00 +0400

    how to do schema:tourModel.js file, more better, with more descriptions

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -5,16 +5,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true,
+    unique: [true, 'A tour must have a duration'],
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
-  rating: {
-    type: Number,
-    default: 4.5,
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 // create model for that schema

94881340b21a6431c8bfc0dcc5fc2ec5768a8a25
anmarkosyan <anush.markos@gmail.com>
2020-12-08 18:43:57 +0400

    how to define aggregation pipeline MongoDB framework

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -115,3 +115,43 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+//define an aggregation pipeline for get a statistics about our tour and manipulate data using calculation
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgRating: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};

8ef1bbd7ae4f62af2718dfe5b887bcfb72209dc4
anmarkosyan <anush.markos@gmail.com>
2020-12-10 17:22:32 +0400

    aggregation middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -101,7 +101,14 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  //console.log(docs);
+  next();
+});
+
+//Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  //console.log(this.pipeline());
   next();
 });
 

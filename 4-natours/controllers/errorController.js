219b094b756626bd2af1190abe6983b30ebfdaf2
anmarkosyan <anush.markos@gmail.com>
2020-12-12 18:17:24 +0400

    here shown how to define error which send to client

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -8,10 +8,22 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  //Operational,trusted error!send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error:don't leak error details
+  } else {
+    //1) log error
+    console.log('ERROR 💥', err);
+    //2)Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something want very wrong!',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {

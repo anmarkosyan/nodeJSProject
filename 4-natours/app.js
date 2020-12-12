d6587b72f7fac34582483b77fff758d608cac33f
anmarkosyan <anush.markos@gmail.com>
2020-12-12 15:54:55 +0400

    created global error handling middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,6 @@
 //how to use express web framework
 const express = require('express');
 const morgan = require('morgan'); // morgan => 3rd party middleware:HTTP request logger middleware for node.js
-
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
 
@@ -37,13 +36,28 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRoute); //1 run
 app.use('/api/v1/users', userRoute); //2 run
 
-//3 run: if this error handler is worked it's mean that our req/res cycle was not yet finished,
+//if this error handler is worked it's mean that our req/res cycle was not yet finished,
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  //create an error , which then will called in error middleware function
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+}); //3 run:
+
+//global error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 module.exports = app;

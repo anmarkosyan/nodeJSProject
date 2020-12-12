50cebd9fb8377116b502b312b98b3cff23069730
anmarkosyan <anush.markos@gmail.com>
2020-12-12 16:16:14 +0400

    require AppError class and use it

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan'); // morgan => 3rd party middleware:HTTP request logger middleware for node.js
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -38,15 +39,7 @@ app.use('/api/v1/users', userRoute); //2 run
 
 //if this error handler is worked it's mean that our req/res cycle was not yet finished,
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-  //create an error , which then will called in error middleware function
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 }); //3 run:
 
 //global error handler

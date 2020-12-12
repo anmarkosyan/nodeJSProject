0dae7e2581a804982fb647ec2cea6da58015bef2
anmarkosyan <anush.markos@gmail.com>
2020-12-12 18:02:28 +0400

    how to define error in development and production environments

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,9 +1,26 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendErrorProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };

8ac75d627010f9d0864e9e98ab2b06d5d9f6129e
anmarkosyan <anush.markos@gmail.com>
2020-12-13 18:12:10 +0400

    handle validator error

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -10,6 +10,12 @@ const handleDupFieldsDB = err => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -49,6 +55,8 @@ module.exports = (err, req, res, next) => {
 
     if (error.kind === 'ObjectId') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDupFieldsDB(error);
+    if (error._message === 'Validation failed')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }

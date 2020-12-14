a0a85b36247ea5572ab6d9b9bf4e45bc9fab1f57
anmarkosyan <anush.markos@gmail.com>
2020-12-14 17:56:21 +0400

    we created our first user data, with signup->post method, which is now in our user database

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const userController = require('./../controllers/userController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 // ROUTES
 // MOUNTING the router
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')

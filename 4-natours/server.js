d53b4de8da1a67dd058a2021c84c31a8f4fb223f
anmarkosyan <anush.markos@gmail.com>
2020-12-14 13:40:46 +0400

    how to catch uncaught exceptions

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -6,6 +6,15 @@ and it's there when we listen to our server.
  */
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+//catch uncaught exceptions: all bugs,that occur in our synchronous code
+//but are not handled anywhere are called uncaught exceptions,
+//and should do this before any process is running
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('Uncaught exception 💥: Shutting down...');
+  //shut down application
+});
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -35,13 +44,13 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-//central place to handle all Unhandled promise rejection,catch  form anywhere in the application
+//central place to handle all UNHANDLED PROMISE REJECTIONS,catch  form anywhere in the application
 process.on('unhandledRejection', err => {
   console.log(err.name, err.message);
   console.log('Unhandled rejection 💥: Shutting down...');
   //1:close the server
   server.close(() => {
-    //2:to shut down application
+    //2:shut down application
     process.exit(1);
   });
 });

2ce5c06388a5326e52ee3868467f74adb082b988
anmarkosyan <anush.markos@gmail.com>
2020-12-19 13:58:14 +0400

    changed user signup way, so prevent a problem with secure signing up

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',

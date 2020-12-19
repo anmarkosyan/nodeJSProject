a02f2700b5fd77a1fdf1841d76a1f0bdd12ff09c
anmarkosyan <anush.markos@gmail.com>
2020-12-19 19:23:53 +0400

    how to create secure login function

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,9 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -20,5 +25,33 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+//logging users in based on user exist and password is correct
+exports.login = catchAsync(async (req, res, next) => {
+  //const email = req.body.email  and const password = req.body.password => using object destructuring with ES6
+  const { email, password } = req.body;
+
+  // 1) check if email and password exist, if not sent an error message
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) check if the user exists && the password is correct
+  const user = await User.findOne({ email }).select('+password');
+  //console.log(user)
+  //password should be 'gbtrrty1' === '$2a$12$XgfT51zj.JuD7izP0ffGqesCfw8YOsJuttjDHDMpQgJX60wMA0j8.'
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password!', 401));
+  }
+
+  // 3) if everything ok send token to the client
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });

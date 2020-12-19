53ad9491df2293d8845e3f8db4b10d8eeec2ddfc
anmarkosyan <anush.markos@gmail.com>
2020-12-19 17:52:51 +0400

    how to get all users

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,11 +1,20 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
 //👤 users route handler
-exports.getAllUsers = (req, res) => {
-  //500 status: internal server error
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not yet defined!',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    //formatted our response using JSend specification
+    status: 'success',
+    result: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
+
 exports.getUser = (req, res) => {
   //500 status: internal server error
   res.status(500).json({

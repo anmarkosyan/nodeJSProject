e6bc801dcc8b5a97aebb30facf63257b102ba7c0
anmarkosyan <anush.markos@gmail.com>
2020-12-20 18:33:03 +0400

    how to handle expired token error

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -16,8 +16,11 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
-const handleJWTError = err =>
+const handleJWTError = () =>
   new AppError('Invalid token! Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -61,6 +64,8 @@ module.exports = (err, req, res, next) => {
     if (error._message === 'Validation failed')
       error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
 
     sendErrorProd(error, res);
   }

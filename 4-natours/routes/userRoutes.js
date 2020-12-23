5f0335b4849ce0f962aaca305a155d71dd8cf2f5
anmarkosyan <anush.markos@gmail.com>
2020-12-23 15:50:38 +0400

    define forgotPassword and resetPassword routes

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -9,6 +9,9 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword', authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)

1d6408dda00d1f1902f51823abf52e98515eb29b
anmarkosyan <anush.markos@gmail.com>
2020-12-25 17:34:44 +0400

    created pre() mongoose middleware function for update passwordChangedAt property

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -60,6 +60,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+//using mongoose middleware for updating passwordChangedAt property
+userSchema.pre('save', function (next) {
+  if (this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
 //create a function which will check if the given password is the same as the one stored in the document
 //first we should create  an instance method: which is basically a method, that is gonna be available on all documents of a certain collection
 userSchema.methods.correctPassword = async function (

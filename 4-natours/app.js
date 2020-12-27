65a0ad6001641fefa7907490ce320fbc21f3e43d
anmarkosyan <anush.markos@gmail.com>
2020-12-27 14:55:51 +0400

    how to data sanitization

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan'); // morgan => 3rd party middleware:HTTP request logger middleware for node.js
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));

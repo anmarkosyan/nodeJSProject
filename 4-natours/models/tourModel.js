5452e76835be211df3e29b39ff3fb9bc3592bd49
anmarkosyan <anush.markos@gmail.com>
2021-01-02 17:06:14 +0400

    Modelling Tour Guides: Embedding

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 //const validator = require('validator');
+const User = require('./userModel');
 
 // create schema for describing tour data
 const tourSchema = new mongoose.Schema(
@@ -104,6 +105,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -121,6 +123,14 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function (next) {
   //console.log(this);
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  //console.log(guides)
+  this.guides = await Promise.all(guidesPromises);
+
   next();
 });
 

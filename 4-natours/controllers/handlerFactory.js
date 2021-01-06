aeeb4491cac67253f07b1848f0e60f5d7dd27fba
anmarkosyan <anush.markos@gmail.com>
2021-01-06 16:47:14 +0400

    how to improving read performance with indexes

--- 4-natours/controllers/handlerFactory.js
+++ 4-natours/controllers/handlerFactory.js
@@ -28,7 +28,7 @@ exports.getAll = Model =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query;
+    const doc = await features.query.explain();
 
     //SEND RESPONSE
     res.status(200).json({
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -118,6 +118,10 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+//tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 //virtual properties
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;

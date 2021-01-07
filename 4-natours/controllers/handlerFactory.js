09000fef625f448f560700a6b16359de0a84040e
anmarkosyan <anush.markos@gmail.com>
2021-01-07 12:33:15 +0400

    commit explain index

--- 4-natours/controllers/handlerFactory.js
+++ 4-natours/controllers/handlerFactory.js
@@ -28,7 +28,8 @@ exports.getAll = Model =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query.explain();
+    //const doc = await features.query.explain();
+    const doc = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({

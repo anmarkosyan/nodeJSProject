6018f4a38a862f1cd1a79bd799af4cd72b655db6
anmarkosyan <anush.markos@gmail.com>
2021-01-09 16:11:33 +0400

    how to work with pug

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -72,7 +72,10 @@ app.use((req, res, next) => {
 //2: 📌 ROUTES
 //routes with pug templates
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Anush',
+  });
 });
 //this is a route middleware
 app.use('/api/v1/tours', tourRoute);

bcbb146cb6c2d40eff083f2fcc63ff518ac87082
anmarkosyan <anush.markos@gmail.com>
2021-01-10 15:43:30 +0400

    1: Building tours overview page

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) get tour data from collection
+  const tours = await Tour.find();
+
+  //2)build template within overview.pug
+  //3) render het template using tour data 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {

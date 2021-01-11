3cda46f62271db8089e2ace20cabb1a0a02d9093
anmarkosyan <anush.markos@gmail.com>
2021-01-11 18:26:26 +0400

    how to use Mapbox configurations in our application

--- 4-natours/public/js/mapbox.js
+++ 4-natours/public/js/mapbox.js
@@ -3,3 +3,11 @@
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYW51c2htYXJrIiwiYSI6ImNranNnYXVqOTI0bWYyeXRmZWxuMTZmcW4ifQ.b2TPwuEqCaha-op4NNC0vA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});

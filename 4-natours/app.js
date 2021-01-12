22ec356a724e0dec393daa5d87b2bfcd92d982a1
anmarkosyan <anush.markos@gmail.com>
2021-01-12 14:05:30 +0400

    added cookieParser in  middleware and test it in cosole.log

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
@@ -42,6 +43,7 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 //Data sanitization against NOSQL query injection
 app.use(mongoSanitize());
@@ -66,7 +68,7 @@ app.use(
 //Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  //console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 

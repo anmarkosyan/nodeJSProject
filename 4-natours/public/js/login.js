b083afa31763878927397278d2ca0fb2ce3ac561
anmarkosyan <anush.markos@gmail.com>
2021-01-12 20:19:36 +0400

    how to make a parcel file

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,26 +1,25 @@
 /* eslint-disable */
 
-import axios from "axios";
+import axios from 'axios';
 
-  export const login = async (email, password) => {
-    try {
-      const res = await axios({
-        method: 'POST',
-        url: 'http://127.0.0.1:3000/api/v1/users/login',
-        data: {
-          email,
-          password,
-        },
-      });
+export const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
 
-      if (res.data.status === 'success') {
-        alert('Logged in successfully');
-        window.setTimeout(() => {
-          location.assign('/');
-        }, 1500);
-      }
-    } catch (err) {
-      alert(err.response.data.message);
+    if (res.data.status === 'success') {
+      alert('Logged in successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
     }
-  };
-
+  } catch (err) {
+    alert(err.response.data.message);
+  }
+};
---
+++ 4-natours/public/js/script.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+//console.log('hello from the parcel!');
+
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login } from './login';
+
+//📌DOM Elements
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+//📌Values
+
+//📌Delegations
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+//create event listener for the submit event on our login form
+if (loginForm) {
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}

90fbe915561b49a302f35963e24a6b45721f0a4a
anmarkosyan <anush.markos@gmail.com>
2021-01-12 19:56:56 +0400

    clean file for exported into script.js

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,32 +1,26 @@
 /* eslint-disable */
 
-const login = async (email, password) => {
-  try {
-    const res = await axios({
-      method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
-      data: {
-        email,
-        password,
-      },
-    });
+import axios from "axios";
 
-    if (res.data.status === 'success') {
-      alert('Logged in successfully');
-      window.setTimeout(() => {
-        location.assign('/');
-      }, 1500);
+  export const login = async (email, password) => {
+    try {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:3000/api/v1/users/login',
+        data: {
+          email,
+          password,
+        },
+      });
+
+      if (res.data.status === 'success') {
+        alert('Logged in successfully');
+        window.setTimeout(() => {
+          location.assign('/');
+        }, 1500);
+      }
+    } catch (err) {
+      alert(err.response.data.message);
     }
-  } catch (err) {
-    alert(err.response.data.message);
-  }
-};
+  };
 
-//create event listener for the submit event on our login form
-document.querySelector('.form').addEventListener('submit', async e => {
-  e.preventDefault();
-
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  await login(email, password);
-});
--- 4-natours/public/js/mapbox.js
+++ 4-natours/public/js/mapbox.js
@@ -1,55 +1,54 @@
 /* eslint-disable */
 //console.log('hello from the client-side 😀');
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiYW51c2htYXJrIiwiYSI6ImNranNnbjhrdTd5OGoyc2xnMm1jOHltZm8ifQ.uShbrbaiJqCjBAHn_xTCqw';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYW51c2htYXJrIiwiYSI6ImNranNnbjhrdTd5OGoyc2xnMm1jOHltZm8ifQ.uShbrbaiJqCjBAHn_xTCqw';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/anushmark/ckjsofvvi5jzz19qg94vxaknd',
+    scrollZoom: false,
+    // center: [-121.908898,37.314025 ],
+    // zoom: 4,
+    //interactive: false,
+  });
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/anushmark/ckjsofvvi5jzz19qg94vxaknd',
-  scrollZoom: false,
-  // center: [-121.908898,37.314025 ],
-  // zoom: 4,
-  //interactive: false,
-});
+  //how to automatically figure out the position of the map based on our tour location points.
+  const bounds = new mapboxgl.LngLatBounds();
+  //loop through all our locations
+  locations.forEach(loc => {
+    //create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-//how to automatically figure out the position of the map based on our tour location points.
-const bounds = new mapboxgl.LngLatBounds();
-//loop through all our locations
-locations.forEach(loc => {
-  //create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    //add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  //add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    //add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  //add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    //extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-  //extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-//fit bounds
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    right: 100,
-    left: 100,
-  },
-});
+  //fit bounds
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      right: 100,
+      left: 100,
+    },
+  });
+};

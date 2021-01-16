08fe482b203d02b87e7b1154614ab17898448f10
anmarkosyan <anush.markos@gmail.com>
2021-01-16 14:50:44 +0400

    how to save uploaded image in the database

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -51,8 +51,8 @@ exports.getMe = (req, res, next) => {
 //======================== 📍User update ==========================
 //allow the currently logged in user to manipulate his user data(email, name)
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
+  //console.log(req.file);
+  //console.log(req.body);
   //1) create error if user POST password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -65,6 +65,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   //2) Filtered out unwanted fields name that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
   //3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,

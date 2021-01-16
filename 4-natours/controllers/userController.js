114cbb25c372a189cbf73fbb9f9ccd433928997d
anmarkosyan <anush.markos@gmail.com>
2021-01-16 15:28:58 +0400

    how to resize uploaded photo

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -6,16 +7,17 @@ const factory = require('./handlerFactory');
 
 //============== 📍Multer middleware usage =================
 //it work not only for uploading photo, but for all kinds of files
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    //user-123cvbn4567fgh-123456788.jpeg
-    const ext = file.mimetype.split('/')[1]; //jpeg
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     //user-123cvbn4567fgh-123456788.jpeg
+//     const ext = file.mimetype.split('/')[1]; //jpeg
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   //check if the uploaded file is an image:  pass true/false in the cb function
@@ -32,6 +34,21 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+//================ 📍Resize user photo middleware ===========
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 //================ 📍Filter Object ===============
 const filterObj = (obj, ...allowedFields) => {

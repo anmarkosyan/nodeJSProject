888d15968583000e86502077e42b50e70fe71451
anmarkosyan <anush.markos@gmail.com>
2021-01-20 18:47:33 +0400

    how to create passwordReset template connect with email.js and call the class method in the authcontroller file, at the end send test mail to mailtrap.io

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -188,19 +188,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   //3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to:
- ${resetURL}.\nIf you didn't forgot your password, please ignore this email!`;
-
   try {
-    // await sendEmail({
-    //   email: req.body.email,
-    //   subject: 'Your password reset token (valid for 10 min)',
-    //   message,
-    // });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -51,4 +51,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours  Family 👋');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid only for 10 minutes!)'
+    );
+  }
 };

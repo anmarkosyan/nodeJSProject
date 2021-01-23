16fd7ae8adf0df5c4ec90a448f81986d79fa6278
anmarkosyan <anush.markos@gmail.com>
2021-01-23 17:03:51 +0400

    comment out CSP from here and put it to the app.js file with helmet middleware

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -32,10 +32,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   //3) render that template using data from step 1)
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
+    // .set(
+    //   'Content-Security-Policy',
+    //   "default-src 'self' https://*.mapbox.com https://*.stripe.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    // )
     .render('tour', {
       title: `${tour.name} Tour`,
       tour,

88c43f73360fa03c1092506601ede6fd5a7c1d2e
anmarkosyan <anush.markos@gmail.com>
2021-01-25 15:56:54 +0400

    before deploy app get rid all URL or API s first part, because our API and website use the same URL,and they are hosted on the same server

--- 4-natours/public/js/stripe.js
+++ 4-natours/public/js/stripe.js
@@ -11,9 +11,7 @@ const stripe = Stripe(
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     //console.log(session);
 
     // 2) Create checkout form + change credit card
--- 4-natours/public/js/updateSettings.js
+++ 4-natours/public/js/updateSettings.js
@@ -8,8 +8,8 @@ export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
 
     const res = await axios({
       method: 'PATCH',
@@ -18,7 +18,7 @@ export const updateSettings = async (data, type) => {
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', `${type.toUpperCase()} updating successfully` );
+      showAlert('success', `${type.toUpperCase()} updating successfully`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
